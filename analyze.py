b7fd8e841d39778bc6eb5a4748cc349ffb332bbc
zizhazhu <zizhazhu@gmail.com>
2017-10-13 16:24:31 +0800

    add analyze

---
+++ analyze.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+
+
+if __name__ == '__main__':
+    val = True
+
+    dataset = pd.read_csv(sys.argv[1])
+    for i in range(1, 29):
+        group_dataset = dataset[dataset['group'] == i]
+        print("Group {} has {} entries".format(i, group_dataset.shape[0]))
+        for j in range(1, 21):
+            tmp_dataset = group_dataset[group_dataset['era'] == j]
+            one = tmp_dataset[tmp_dataset['label'] == 1]
+            zero = tmp_dataset[tmp_dataset['label'] == 0]
+            print("Group:{} Era:{} 1:{} 0:{}".format(i, j, one.shape[0], zero.shape[0]))
