09553990aa0b180290aa4fd7d308168ab19b584b
zizhazhu <zizhazhu@gmail.com>
2017-10-13 16:28:16 +0800

    remove weight

--- preprocess/split.py
+++ preprocess/split.py
@@ -5,7 +5,6 @@ def data_split(dataset, val=True, part=None):
     # weight determine score
     if part is not None:
         dataset = dataset[dataset['group'] == part]
-    weight = dataset['weight']
     dataset = dataset.drop(['feature77', 'id', 'weight', 'group'], axis=1)
     y = dataset['label'].astype('int')
     dataset = dataset.drop('label', axis=1)
@@ -15,7 +14,6 @@ def data_split(dataset, val=True, part=None):
         valid_idx = dataset['era'] == 20
         x_train, x_valid = x[train_idx], x[valid_idx]
         y_train, y_valid = y[train_idx], y[valid_idx]
-        weight_train, weight_valid = weight[train_idx], weight[valid_idx]
-        return x_train, x_valid, y_train, y_valid, weight_train, weight_valid
+        return x_train, x_valid, y_train, y_valid
     else:
-        return x, y, weight
+        return x, y
