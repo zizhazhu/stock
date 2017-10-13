f8a6f82ff760af680d9b8a7a8356a66c69ffdee5
zizhazhu <zizhazhu@gmail.com>
2017-10-13 16:24:45 +0800

    add weight

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ if __name__ == '__main__':
     val = True
 
     train_data = pd.read_csv(sys.argv[1])
-    result = split.data_split(train_data, val=val, part=True)
+    result = split.data_split(train_data, val=val)
     if val:
-        x_train, x_valid, y_train, y_valid = result
+        x_train, x_valid, y_train, y_valid, weight_train, weight_valid = result
     else:
-        x_train, y_train = result
+        x_train, y_train, weight = result
         test_data = pd.read_csv(sys.argv[2])
         id_test = test_data['id']
         test_data.drop(['id', 'group', 'feature77'], axis=1, inplace=True)
         x_valid = test_data
 
-    clf = RandomForestClassifier(n_estimators=100, max_depth=10, n_jobs=2)
+    clf = RandomForestClassifier(n_estimators=100, max_depth=10, n_jobs=2, class_weight=weight_train)
     clf.fit(x_train, y_train)
 
     if val:
--- preprocess/split.py
+++ preprocess/split.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 
-def data_split(dataset, val=True, part=False):
+def data_split(dataset, val=True, part=None):
     # weight determine score
-    if part:
-        dataset = dataset[dataset['group'] == 1]
-    dataset = dataset.drop(['feature77', 'weight', 'id', 'group'], axis=1)
+    if part is not None:
+        dataset = dataset[dataset['group'] == part]
+    weight = dataset['weight']
+    dataset = dataset.drop(['feature77', 'id', 'weight', 'group'], axis=1)
     y = dataset['label'].astype('int')
     dataset = dataset.drop('label', axis=1)
     x = dataset
@@ -14,6 +15,7 @@ def data_split(dataset, val=True, part=False):
         valid_idx = dataset['era'] == 20
         x_train, x_valid = x[train_idx], x[valid_idx]
         y_train, y_valid = y[train_idx], y[valid_idx]
-        return x_train, x_valid, y_train, y_valid
+        weight_train, weight_valid = weight[train_idx], weight[valid_idx]
+        return x_train, x_valid, y_train, y_valid, weight_train, weight_valid
     else:
-        return x, y
+        return x, y, weight
