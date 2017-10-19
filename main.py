e3eb84abbca47e39773b5b8e9ee5ffd64433e740
zizhazhu <zizhazhu@gmail.com>
2017-10-19 14:36:38 +0800

    add MinMaxScaler

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
 from preprocess import split
 
 
@@ -11,7 +11,7 @@ def Score(y_pred, y_true, weight):
 
 
 if __name__ == '__main__':
-    val = False
+    val = True
 
     train_data = pd.read_csv(sys.argv[1])
     result = split.data_split(train_data, val=val, part=None)
@@ -22,11 +22,13 @@ if __name__ == '__main__':
         test_data = pd.read_csv(sys.argv[2])
         id_test = test_data['id']
         test_data = split.one_hot_extend(test_data, 'group', remove=True)
-        test_data = test_data.drop(['id', 'feature77'], axis=1)
         test_data = test_data.drop(['id', 'feature43'], axis=1)
         x_valid = test_data
 
-    clf = RandomForestClassifier(n_estimators=200, max_depth=6, oob_score=True, n_jobs=-3)
+    Scaler = MinMaxScaler()
+    Scaler.fit_transform(x_train)
+    Scaler.transform(x_valid)
+    clf = RandomForestClassifier(n_estimators=200, max_depth=2, oob_score=True, n_jobs=-2)
     clf.fit(x_train, y_train)
 
     prob = clf.predict_proba(x_valid)[:, 1]
